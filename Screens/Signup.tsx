bc061f027c7bf9989b24b1d01abe3b3a14d2b986
Mustafa alhasan <mustafa.alhasan.2020@gmail.com>
2024-07-07 16:42:40 +0300

    Vendor App finished

--- Screens/Signup.tsx
+++ Screens/Signup.tsx
@@ -12,6 +12,7 @@ import { setAuthData, setAesKey } from '../Redux/slices/authSlice';
 import baseUrl from '../baseUrl';
 import { generateAesKey, encryptAesKey, encryptData, decryptData } from '../crypto-utils';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import FeatherIcon from 'react-native-vector-icons/Feather'; // Import Feather icons
 import DateTimePicker from '@react-native-community/datetimepicker';
 import FuseLogo from '../assets/FuseLogo.png';
 import WhiteLogo from '../assets/White-Logo-PNG.png';
@@ -46,6 +47,10 @@ const Signup = () => {
   const [loading, setLoading] = useState(false);
   const [passwordVisible, setPasswordVisible] = useState(false);
   const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
+  const [hasEightChars, setHasEightChars] = useState(false);
+  const [hasCapitalLetter, setHasCapitalLetter] = useState(false);
+  const [hasNumber, setHasNumber] = useState(false);
+  const [hasSpecialChar, setHasSpecialChar] = useState(false);
 
   const backgroundColor = theme === 'light' ? '#FFFFFF' : '#303030';
   const textColor = theme === 'light' ? '#1F1F1F' : '#FFFFFF';
@@ -97,7 +102,7 @@ const Signup = () => {
 
     setLoading(true);
     try {
-      const payload = encryptData({ email, name, password, phone, birth: formatDate(birth), role: 'Vendor',monthlyIncome:'',category:'' }, aesKey);
+      const payload = encryptData({ email, name, password, phone, birth: formatDate(birth), role: 'Vendor', monthlyIncome: '', category: '' }, aesKey);
       const response = await axios.post(`${baseUrl}/auth/register`, { email, payload });
       const decryptedPayload = decryptData(response.data.payload, aesKey);
 
@@ -228,7 +233,13 @@ const Signup = () => {
               <TextInput
                 style={[tw`flex-row w-grow`]}
                 value={password}
-                onChangeText={setPassword}
+                onChangeText={(text) => {
+                  setPassword(text);
+                  setHasEightChars(text.length >= 8);
+                  setHasCapitalLetter(text.match(/[A-Z]/g) ? true : false);
+                  setHasNumber(text.match(/[0-9]/g) ? true : false);
+                  setHasSpecialChar(text.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g) ? true : false);
+                }}
                 secureTextEntry={!passwordVisible}
                 textContentType="newPassword"
                 autoComplete="new-password"
@@ -237,6 +248,24 @@ const Signup = () => {
               <TouchableOpacity onPress={() => setPasswordVisible(!passwordVisible)} style={{ padding: 8 }}>
                 <Icon name={passwordVisible ? 'eye-off' : 'eye'} size={24} color={placeholderColor} />
               </TouchableOpacity>
+            </View>
+            <View style={tw`mb-2 -mt-2`}>
+              {!hasEightChars && <View style={tw`flex-row items-center `}>
+                <FeatherIcon name={hasEightChars ? "check" : "x"} size={15} color={textColor} />
+                <Text style={[tw`text-xs ml-1`, { color: textColor }]}>Password must be at least 8 characters long</Text>
+              </View>}
+              {!hasCapitalLetter && <View style={tw`flex-row items-center `}>
+                <FeatherIcon name={hasCapitalLetter ? "check" : "x"} size={15} color={textColor} />
+                <Text style={[tw`text-xs ml-1`, { color: textColor }]}>Password must have at least one capital letter</Text>
+              </View>}
+              {!hasNumber && <View style={tw`flex-row items-center `}>
+                <FeatherIcon name={hasNumber ? "check" : "x"} size={15} color={textColor} />
+                <Text style={[tw`text-xs ml-1`, { color: textColor }]}>Password must be at least one number</Text>
+              </View>}
+              {!hasSpecialChar && <View style={tw`flex-row items-center `}>
+                <FeatherIcon name={hasSpecialChar ? "check" : "x"} size={15} color={textColor} />
+                <Text style={[tw`text-xs ml-1`, { color: textColor }]}>Password must have at least one special character</Text>
+              </View>}
             </View>
 
             <Text style={[tw`text-sm pl-2 mb-1`, { color: textColor }]}>Confirm Password</Text>
